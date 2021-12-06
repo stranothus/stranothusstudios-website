d6054e0e274b4c2bf7233a38a385d25b213a79c6
stranothus <stranothus@gmail.com>
2021-12-06 14:56:38 -0600

    Sticky position instead of scroll event Use a sticky position instead of a scroll event. Scroll events fire poorly on mobile devices and can be unreliable.

--- public/scripts/nav.js
+++ public/scripts/nav.js
@@ -9,23 +9,5 @@ fetch("/api/nav")
 
 	nav.replaceWith(newNav);
 	window.scroll(0, 0);
-	let navTop = newNav.offsetTop;
-
-	window.addEventListener("scroll", function() {
-		if(document.documentElement.scrollTop > navTop) {
-			newNav.style.position = "fixed";
-			newNav.style.boxShadow = "0 5px 5px 0 #000000A0";
-		} else {
-			newNav.style.position = "relative";
-			newNav.style.boxShadow = "none";
-		}
-	});
-	
-	window.addEventListener("resize", function() {
-	    let scroll = document.documentElement.getBoundingClientRect().top;
-	    window.scroll(0, 0);
-	    navTop = newNav.offsetTop;
-	    window.scroll(0, -scroll);
-	});
 })
 .catch(err => console.log(err));
